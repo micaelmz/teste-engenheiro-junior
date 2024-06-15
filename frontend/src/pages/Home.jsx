a9c19de437d21cdf02ab4f0038c0731b279829c9
micaelmz <micaelmuniz6@gmail.com>
2024-06-15 02:50:52 -0300

    fix. ajustando centralizaçao vertical da pagina utilizando css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,19 +9,22 @@ import {Button} from "react-bootstrap";
 
 export default function Home() {
   return (
-      <Container className="container-fullvh">
-        <Row>
-            <Col xs={6}>
-              <div className="d-flex justify-content-center h-100 flex-column">
-                <p className="text-white poppins fw-bold fs-1">
-                  Ordenai: seu sistema de gerenciamento de pedidos online organizado e eficiente!
-                </p>
-                <Link to="/login"><Button className="w-25 btn-color-1 mt-4">Fazer Login</Button></Link>
+      <Container className="vh-100 d-flex align-items-center">
+        <Row className="w-100">
+          <Col xs={6}>
+            <div className="d-flex justify-content-center h-100 flex-column">
+              <p className="text-white poppins fw-bold fs-1">
+                Ordenai: seu sistema de gerenciamento de pedidos online organizado e eficiente!
+              </p>
+              <div className="flex">
+                <Link to="/login"><Button className="w-25 btn-color-1 mt-4 me-4">Fazer Login</Button></Link>
+                <Link to="/register"><Button className="w-25 btn-color-2 mt-4">Cadastrar-se</Button></Link>
               </div>
-            </Col>
-            <Col xs={6}>
-              <img src={shopIllustration} alt="Shop Ilustration" className="d-flex mx-auto img-fluid w-75"/>
-            </Col>
+            </div>
+          </Col>
+          <Col xs={6}>
+            <img src={shopIllustration} alt="Shop Ilustration" className="d-flex mx-auto img-fluid w-75"/>
+          </Col>
         </Row>
       </Container>
   );
