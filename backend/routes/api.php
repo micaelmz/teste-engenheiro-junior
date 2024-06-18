e40a89d004379db6a6b6968f6f7c307675929669
micaelmz <micaelmuniz6@gmail.com>
2024-06-18 02:09:32 -0300

    feat. rota de deletar um pedido

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -38,4 +38,4 @@ Route::get('/orders', [OrderController::class, 'index']);
 //Route::get('/orders/{id}', [OrderController::class, 'show']);
 Route::post('/orders', [OrderController::class, 'store']);
 Route::put('/orders/{id}', [OrderController::class, 'update']);
-//Route::delete('/orders/{id}', [OrderController::class, 'destroy']);
+Route::delete('/orders/{id}', [OrderController::class, 'destroy']);
