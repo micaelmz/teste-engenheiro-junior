cf1e584280723f9eda9f79ccdaf090aae4eb0c5e
micaelmz <micaelmuniz6@gmail.com>
2024-06-18 06:50:56 -0300

    feat. implementando rotas para pesquisa e estatisticas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -23,22 +23,26 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/clients', [ClientController::class, 'index']);
+Route::get('/clients/search', [ClientController::class, 'search']);
 Route::get('/clients/total', [ClientController::class, 'total']);
-//Route::get('/clients/{id}', [ClientController::class, 'show']);
+Route::get('/orders/tail/{quantity}', [OrderController::class, 'tail']);
 Route::post('/clients', [ClientController::class, 'store']);
 Route::put('/clients/{id}', [ClientController::class, 'update']);
 Route::delete('/clients/{id}', [ClientController::class, 'destroy']);
 
 Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/search', [ProductController::class, 'search']);
 Route::get('/products/total', [ProductController::class, 'total']);
-//Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::post('/products', [ProductController::class, 'store']);
 Route::put('/products/{id}', [ProductController::class, 'update']);
 Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 
 Route::get('/orders', [OrderController::class, 'index']);
+Route::get('/orders/search', [OrderController::class, 'search']);
 Route::get('/orders/total', [OrderController::class, 'total']);
-//Route::get('/orders/{id}', [OrderController::class, 'show']);
+Route::get('/orders/total/price', [OrderController::class, 'totalPrice']);
+Route::get('/orders/tail/{quantity}', [OrderController::class, 'tail']);
+Route::get('/orders/weeksales', [OrderController::class, 'weeksales']);
 Route::post('/orders', [OrderController::class, 'store']);
 Route::put('/orders/{id}', [OrderController::class, 'update']);
 Route::delete('/orders/{id}', [OrderController::class, 'destroy']);
